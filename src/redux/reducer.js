5188f8b475c8a640ce3403a5212243b4bddee2ad
SeungHwan-Lee <sh981013s@gmail.com>
2021-12-12 01:23:29 +0900

    add & remove todos reducers

---
+++ src/redux/reducer.js
@@ -0,0 +1,20 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const addTodoReducer = createSlice({
+    name: 'todos',
+    initialState,
+    reducers: {
+        addTodos: (state,action) => {
+            state.push(action.payload);
+            return state;
+        },
+        removeTodos: (state,action) => {
+            return state.filter(item => item.id !== action.payload);
+        }
+    }
+})
+
+export const {addTodos, removeTodos} = addTodoReducer.actions;
+export const reducer  = addTodoReducer.reducer;
